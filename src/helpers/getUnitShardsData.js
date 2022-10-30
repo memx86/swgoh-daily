9acd9df2740a5d38ca521333c6abdd8362e9628a
memx86 <memx86@gmail.com>
2022-10-30 18:41:17 +0200

    getUnitSahrdsData fleet and cantina fix encounter letters

--- src/helpers/getUnitShardsData.js
+++ src/helpers/getUnitShardsData.js
@@ -5,19 +5,44 @@ export const ENCOUNTERS = {
   C01SP: 'Fleet battles',
   LIGHT: 'C01L',
   DARK: 'C01D',
+  FLEET: 'C01SP',
+  CANTINA: 'C01H',
 };
 
 const getNodeLetter = ({ campaignId, nodeTier, campaignMissionId }) => {
-  if (campaignId === 'C01SP') {
+  if (campaignId === ENCOUNTERS.CANTINA) {
     switch (campaignMissionId) {
       case 'Mi01':
         return 'A';
-      case 'Mi03':
+      case 'Mi02':
         return 'B';
-      case 'Mi05':
+      case 'Mi03':
         return 'C';
-      case 'Mi06':
+      case 'Mi04':
         return 'D';
+      case 'Mi05':
+        return 'E';
+      case 'Mi06':
+        return 'F';
+      case 'Mi07':
+        return 'G';
+      default:
+        return campaignMissionId;
+    }
+  }
+
+  if (campaignId === ENCOUNTERS.FLEET) {
+    switch (campaignMissionId) {
+      case 'Mi01':
+        return 'A';
+      case 'Mi02':
+        return 'B';
+      case 'Mi03':
+        return 'C';
+      case 'Mi04':
+        return 'D';
+      case 'Mi05':
+        return 'E';
       default:
         return campaignMissionId;
     }
@@ -38,19 +63,81 @@ const getNodeLetter = ({ campaignId, nodeTier, campaignMissionId }) => {
     }
   }
 
+  if (nodeTier >= 4 && nodeTier <= 5) {
+    switch (campaignMissionId) {
+      case 'Mi03':
+        return 'A';
+      case 'Mi04':
+        return 'B';
+      case 'Mi07':
+        return 'C';
+      case 'Mi08':
+        return 'D';
+      case 'Mi11':
+        return 'E';
+      case 'Mi12':
+        return 'F';
+      default:
+        return campaignMissionId;
+    }
+  }
+
+  if (nodeTier === 6) {
+    switch (campaignMissionId) {
+      case 'Mi04':
+        return 'A';
+      case 'Mi05':
+        return 'B';
+      case 'Mi08':
+        return 'C';
+      case 'Mi09':
+        return 'D';
+      case 'Mi10':
+        return 'E';
+      default:
+        return campaignMissionId;
+    }
+  }
+
+  if (nodeTier === 7) {
+    switch (campaignMissionId) {
+      case 'Mi03':
+        return 'A';
+      case 'Mi04':
+        return 'B';
+      case 'Mi07':
+        return 'C';
+      case 'Mi08':
+        return 'D';
+      default:
+        return campaignMissionId;
+    }
+  }
+
+  if (nodeTier === 8) {
+    switch (campaignMissionId) {
+      case 'Mi03':
+        return 'A';
+      case 'Mi04':
+        return 'B';
+      case 'Mi07':
+        return 'C';
+      case 'Mi08':
+        return 'D';
+      default:
+        return campaignMissionId;
+    }
+  }
+
   switch (campaignMissionId) {
-    case 'Mi03':
+    case 'Mi01':
       return 'A';
-    case 'Mi04':
+    case 'Mi02':
       return 'B';
-    case 'Mi07':
+    case 'Mi03':
       return 'C';
-    case 'Mi08':
+    case 'Mi04':
       return 'D';
-    case 'Mi11':
-      return 'E';
-    case 'Mi12':
-      return 'F';
     default:
       return campaignMissionId;
   }
