d3249474e8c4aa50fb16e791a1a997c8a88d8f0e
memx86 <memx86@gmail.com>
2022-11-03 23:39:38 +0200

    updateCachedCharacters multiply updates fix

--- src/services/updateCachedCharacters.js
+++ src/services/updateCachedCharacters.js
@@ -3,11 +3,25 @@ import { getCachedCharacters } from './getCachedCharacters';
 import { getCachedCharactersById } from './getCachedCharactersById';
 import { getCharactersUnitShards } from './getCharactersUnitShards';
 
-export const updateCachedCharacters = async () => {
-  const characters = await getCharactersUnitShards();
+const state = {
+  isUpdating: false,
+};
 
+export const updateCachedCharacters = async () => {
+  if (state.isUpdating) return;
+
+  state.isUpdating = true;
+
+  const characters = await getCharactersUnitShards();
   const cachedCharaters = await getCachedCharacters();
+
   const id = cachedCharaters?.id;
   const docToUpdate = getCachedCharactersById(id);
-  await updateDoc(docToUpdate, { characters, updatedAt: Date.now() });
+
+  await updateDoc(docToUpdate, {
+    characters,
+    updatedAt: Date.now(),
+  });
+
+  state.isUpdating = false;
 };
