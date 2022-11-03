786f0817a005cf678fde1d3d8299a4404f0e657b
memx86 <memx86@gmail.com>
2022-11-03 23:56:24 +0200

    user timezone offset placeholder

--- src/helpers/getLocationsAndTries.js
+++ src/helpers/getLocationsAndTries.js
@@ -3,10 +3,11 @@ export const getLocationsAndTries = (locations, tries) => {
 
   locations?.forEach((_, idx) => {
     const currentTry = tries[idx] ?? {};
-    const today = Date.now();
+    const hour = 1000 * 60 * 60;
+    const today = Date.now() + 1 * hour;
     const updated = currentTry.updatedAt ?? 0;
 
-    const isLessThanDay = today - updated < 1000 * 60 * 60 * 24;
+    const isLessThanDay = today - updated < hour * 24;
     const isFresh =
       isLessThanDay &&
       new Date(today).getDate() === new Date(updated).getDate();
